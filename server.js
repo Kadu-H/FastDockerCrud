5d57741be0cfd75f472d8ca91b8d80b13dcaf15f
Kdu <olocgame@gmail.com>
2025-07-20 18:21:31 -0300

    Criando um arquivo de servidor base
    Fiz a instalação do framework Fastify, criei um arquivo servidor base, com uma rota GET de teste

---
+++ server.js
@@ -0,0 +1,21 @@
+import fastify from "fastify";
+
+const startServer = () => {
+    const server = fastify({
+        logger: true,
+    });
+
+    server.get("/", (req, res) => {
+        return res.send({ msg: "Hello World" });
+    })
+
+    server.listen({ port: 3000 }, (err, address) => {
+        if (err) {
+            server.log.error(err);
+            process.exit(1);
+        }
+        console.log(`Servidor rodando em: ${address}`);
+    })
+}
+
+startServer();
