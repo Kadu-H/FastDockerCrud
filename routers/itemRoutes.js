08dc78723655284916ef89d3b5b768f37ffd6eb5
Kdu <83893756+Kadu-H@users.noreply.github.com>
2025-08-03 17:11:33 -0300

    Delete routers directory

--- routers/itemRoutes.js
+++
@@ -1,9 +0,0 @@
-import { listAllItems, showItemById, create, remove, update } from "../controllers/itemController.js";
-
-export default async function itemsRoutes(fastify) {
-    fastify.get('/items', listAllItems);
-    fastify.post('/items', create);
-    fastify.get('/items/:id', showItemById);
-    fastify.put('/items/:id', update);
-    fastify.delete('/items/:id', remove);
-}
